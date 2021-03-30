0675e7a8e204709f2725dce7d17a4845931bee8a
OmidSoroush <omidsoroush@t-online.de>
2021-03-30 15:10:43 +0200

    Added a syntax to plot the occurrence of Corona in the tweets

--- rtweet.R
+++ rtweet.R
@@ -5,14 +5,18 @@ library(plyr)
 library(dplyr)
 library(openxlsx)
 library(tidyr)
+library(ggplot2)
+
+# Please set working directory
+setwd("")
 
 
 # Insert the Credentials you received from Twitter after setting up developer account
-appname <- ""
-key <- ""
-secret <- ""
-access_token <- ""
-access_secret <- ""
+appname <- "tutorial_rtweet"
+key <- "VUzUawDI1JcOkG2VKB8ShOgPv"
+secret <- "JOgmLJGmI5ecpWyadky1qXntPnrQGCmpuYNlBfJRrCzUc5QMXm"
+access_token <- "1527778656-5qoJOil54qiGdUDQfoVwSNRDfpA9vjujD7Q8d3o"
+access_secret <- "AjgnDFdli6mzVKFtt54MNruQ6f6J5D1Nhf7uA2UCWmZPD"
 
 
 # User the Credentials to create authorization token
@@ -43,9 +47,55 @@ timeline$date <- as.Date(timeline$date_time)
 timeline$time <- format(as.POSIXct(timeline$date_time) ,format = "%H:%M:%S") 
 timeline$date_time <- NULL
 
+# Filter the tweets for March 2021
+german_trade_unions <- german_trade_unions %>% filter(date >= "2020-03-01" & date <= "2020-03-31")
+
+# Extract the year from date
+timeline$day <- format(timeline$date, "%d")
+
 # Order variables
 timeline <- timeline %>% select(status_id, created_at, date, time, everything())
 
+# look for "Corona/Covid" in the tweet text
+timeline$corona <- grepl("corona|covid", timeline$text)
+
+# Count the occurrence of "Corona/Covid" by year for CNN and BBC
+tbl_freq <- timeline %>% group_by(day, screen_name) %>%
+  summarise(count = sum(corona == TRUE))
+
+# plot the abs. frequency of "Corona/Covid"
+ggplot(data=tbl_freq, aes(x=day, y=count, group=screen_name)) +
+  geom_line(aes(linetype=screen_name, color=screen_name)) +
+  geom_point(aes(color=screen_name)) +
+  xlab("Month (03/2021)") +
+  ylab("Abs. Frequency") +
+  scale_color_manual(values = c("#ff0000", "#00ffff")) +
+  scale_linetype_manual(values = c("dashed", "solid")) +
+  theme_minimal()
+
+
+
+# After plotting the abs. Frequency, let's plot the relative freq of corona occurrence 
+# Count number of all tweets by year 
+tbl_freq_total_tweets <- timeline %>% group_by(day, screen_name) %>%
+  summarise(all_tweet = length(text))
+
+# Add the total column to the table for relative frequency estimation
+tbl_freq$total_tweet <- tbl_freq_total_tweets$all_tweet
+
+# Estimate the proportion relative to all tweets per year
+tbl_freq$rel_freq <- (tbl_freq$count / tbl_freq$total_tweet) * 100
+tbl_rel_freq <- tbl_freq
+
+# Plot the relative frequency of "Corona" occurrence
+ggplot(data=tbl_rel_freq, aes(x=day, y=rel_freq, group=screen_name)) +
+  geom_line(aes(linetype=screen_name, color=screen_name)) +
+  geom_point(aes(color=screen_name)) +
+  xlab("Month (03/2021)") +
+  ylab("Rel. Frequency") +
+  scale_color_manual(values = c("#ff0000", "#00ffff")) +
+  scale_linetype_manual(values = c("dashed", "solid")) +
+  theme_minimal()
 
 # Write the data in excel workbook
 write.xlsx(timeline, file = "timeline.xlsx",
